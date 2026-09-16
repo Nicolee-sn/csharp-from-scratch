d65ac9f90d2e6122a250ae5b441a5c5611dddd1a
Nicole Sanmartin <sanmartinnciole16@gmail.com>
2026-09-16 12:21:01 -0500

    Add class structure, variables and constants

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "John";
+            name = "John Doe";
+
+            const string greeting = "Hola";
+
+            Console.WriteLine($"{greeting} {name}");
+            Console.ReadLine();
+        }
+    }
+}
